2648062fd4711d6f8485b23a622797b8467c7423
Cai Yuanqing <Yuanqing.Cai@tieto.com>
2011-03-04 09:45:47 +0800

    Python Challenge
    Level 3: Change solution

--- 4puzzle.py
+++ 4puzzle.py
@@ -1,49 +1,16 @@
 #!/usr/bin/env python
 
 import string
+import re
 
-realist = ""
-le = []
-def checkletter(le):
-    for letter in le[:1]:
-        if letter.isupper():
-            return False
-    for letter in le[1:4]:
-        if letter.islower():
-            return False
-    for letter in le[4:5]:
-        if letter.isupper():
-            return False
-    for letter in le[5:8]:
-        if letter.islower():
-            return False
-    for letter in le[8:]:
-        if letter.isupper():
-            return False
-    global realist
-    realist += le[4]
-    return True
-
-def test(a = 'puzzle3'):
+def test(a = 'puzzle4'):
     '''Find characters match the condition .
 
     find letter XXXxXXX'''
     fin = open(a).read()
-    chars = ''
-#    for line in fin:
-#        word = line.strip()
-    for ch in fin:
-        if ch != '\n':
-            le.append(ch)
-        if len(le) == 9:
-            if checkletter(le):
-                print le
-                chars = "".join(le)
-                print chars
-                le.pop(0)
-            else:
-                le.pop(0)
-    print realist
+    chars = "".join(re.findall('(?:^|[^A-Z])[A-Z]{3}([a-z])[A-Z]{3}(?:[^A-Z]|$)',fin))
+    print chars
+
 
 def __main__():
     print test.__doc__
